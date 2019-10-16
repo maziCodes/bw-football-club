bf353ab708532baed786f20c4b8424782ac94837
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-16 03:37:42 +0100

    add teams component

--- src/app/competitions/competitions.module.ts
+++ src/app/competitions/competitions.module.ts
@@ -5,12 +5,14 @@ import { CompetitionsRoutingModule } from './competitions.routing.module';
 import { CompetitionsComponent } from './competitions.component';
 import { FixturesComponent } from './fixtures/fixtures.component';
 import { TableComponent } from './table/table.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     CompetitionsComponent,
     FixturesComponent,
-    TableComponent
+    TableComponent,
+    TeamsComponent
   ],
   imports: [
     CommonModule,

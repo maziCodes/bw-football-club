c42927d96006dd9b8b278e6786025c7650ea4f83
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-16 02:26:52 +0100

    feature: complete fixtures view

--- src/app/competitions/competitions.routing.module.ts
+++ src/app/competitions/competitions.routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CompetitionsComponent } from './competitions.component';
+import { FixturesComponent } from './fixtures/fixtures.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CompetitionsComponent,
-    // children: [
-    //   // { path: '', redirectTo: 'fixtures' },
-    // ]
+    children: [
+      { path: '', redirectTo: 'fixtures/none' },
+      { path: 'fixtures/:id', component: FixturesComponent}
+    ]
   }
 ];
 

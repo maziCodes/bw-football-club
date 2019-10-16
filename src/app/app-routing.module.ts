4af4bedeadd3c82cab53a5a12983f6c5e419410d
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-16 02:35:20 +0100

    feature: add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'competitions' },
   { path: 'competitions', loadChildren: './competitions/competitions.module#CompetitionsModule' },
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({

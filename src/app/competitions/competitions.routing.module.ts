92a2d71aac8128a4777532a5422f56a935fabd7b
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-16 09:21:32 +0100

    feature: complete single-team view

--- src/app/competitions/competitions.routing.module.ts
+++ src/app/competitions/competitions.routing.module.ts
@@ -5,6 +5,7 @@ import { CompetitionsComponent } from './competitions.component';
 import { FixturesComponent } from './fixtures/fixtures.component';
 import { TableComponent } from './table/table.component';
 import { TeamsComponent } from './teams/teams.component';
+import { TeamComponent } from './teams/team/team.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,7 @@ const routes: Routes = [
       { path: 'fixtures/:id', component: FixturesComponent},
       { path: 'table/:id', component: TableComponent},
       { path: 'teams/:id', component: TeamsComponent}
+      { path: 'team/:id', component: TeamComponent}
     ]
   }
 ];

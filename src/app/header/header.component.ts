ac79f3eaaef2f3f82248a2d48be378fe08e84892
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-17 01:57:16 +0100

    feature: get the teams from any competition in the header and route to component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,26 +11,61 @@ export class HeaderComponent implements OnInit {
 
   dropDownEl = document.getElementsByClassName('drop-down');
   competitions: [];
+  teams: any;
+  callLimit = 1;
+  activeCompetition: any;
 
   constructor(private _appService: AppService) { }
 
   ngOnInit() {
-    this.fetchCompetitions();
+    this.competitions = JSON.parse(localStorage.getItem('competitions'));
+
+    if (!this.competitions) {
+      this.fetchData(this.competitionsUrl, 'competitions');
+      console.log('call api');
+    }
   }
 
-  fetchCompetitions() {
-    this._appService.fecthData(this.competitionsUrl)
-    .subscribe( (data: object) => {
-      console.log('competitions', data);
-      this.competitions = data['competitions'];
-
+  fetchData(url, type?) {
+    this._appService.fecthData(url).subscribe( (data: object) => {
+      console.log('api data', data);
+      this.assignPeoperty(data, type);
     }, err => {
       console.log(err);
     });
   }
 
-  showDropDown() {
-   this.dropDownEl[0]['style'].display = 'block';
+  // switch properties
+  assignPeoperty(data, typeOfCall) {
+
+    switch (typeOfCall) {
+      case 'competitions':
+        this.competitions = data[typeOfCall];
+        // store object to reduce api call
+        localStorage.setItem('competitions', JSON.stringify(this.competitions));
+        break;
+
+      case 'teams':
+        this.activeCompetition = data['competition'];
+        this.teams = data['teams'];
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  showDropDown(competitionCode?) {
+    const url = `competitions/${competitionCode}/teams`;
+
+    this.dropDownEl[0]['style'].display = 'block';
+
+    if (this.callLimit === 1) {
+      this.fetchData(url, 'teams');
+    }
+
+    this.callLimit = this.callLimit + 1;
+
   }
 
   closeDropDown() {

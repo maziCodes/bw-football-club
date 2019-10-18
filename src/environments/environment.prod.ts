ee2cb73858b60b6babb4833af83912bdd0faf297
Emeka Nwachukwu <bristonmekus@gmail.com>
2019-10-18 17:29:09 +0100

    chore: update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
   production: true,
+  baseUrl: 'https://api.football-data.org/v2/',
   headerToken: 'f76a97c0bad842ebaf7981a70d58355e'
 };

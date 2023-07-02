2234ff7c39f07baedbdda8c8d1f823309e8fccf9
mazicodes <bristonmekus@gmail.com>
2023-07-02 06:25:07 +0100

    update: envelope variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
   production: true,
-  baseUrl: 'https://api.football-data.org/v2/',
-  headerToken: 'f76a97c0bad842ebaf7981a70d58355e'
+  baseUrl: 'https://api.football-data.org',
+  headerToken: 'f76a97c0bad842ebaf7981a70d58355e',
+  footballDataVersion: import.meta.env.NG_APP_FOOTBALL_DATA_VERSION
 };
